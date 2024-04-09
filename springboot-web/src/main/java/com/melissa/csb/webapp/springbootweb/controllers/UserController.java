7a39c86a93c8bcee4e53877d78da5a8f4598879e
DevMGcode <meli.bogar15@gmail.com>
2024-04-09 14:10:03 -0500

    Pasar Datos a la vista

--- springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/UserController.java
+++ springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/UserController.java
@@ -1,14 +1,18 @@
 package com.melissa.csb.webapp.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller // manejará  solicitudes HTTP entrantes
 public class UserController {
 
-  @GetMapping("/details")
-  public String details(){
-
-    return "details";
+  @GetMapping("/details")// mapear solicitudes HTTP GET a metodos controladores especificos en la clase
+                      //(Map<String, Objetct> model)
+  public String details(Model model){ //objeto Model como argumento que se utiliza para agregar atributos que pasaran a la vista
+    model.addAttribute("title", "Hola Spring-Boot");
+    model.addAttribute("name", "Melissa");
+    model.addAttribute("lastname", "Garcia");
+    return "details";//nombre de la vista a renderizar
   }
 }
