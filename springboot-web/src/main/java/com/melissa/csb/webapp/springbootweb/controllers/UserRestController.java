9808ec2af7c41f6a615938821cd434c5dbef4637
DevMGcode <meli.bogar15@gmail.com>
2024-04-09 14:46:19 -0500

    Anotación @RequestMapping

--- springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/UserRestController.java
@@ -1,13 +1,27 @@
 package com.melissa.csb.webapp.springbootweb.controllers;
 import java.util.HashMap;
 import java.util.Map;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+
+
+
+//@Controller opcional 2
 @RestController // manejará  solicitudes HTTP entrantes
+@RequestMapping("/api")
+
 public class UserRestController {
 
-  @GetMapping("/details2")// mapear solicitudes HTTP GET a metodos controladores especificos en la clase
+  //@RequestMapping(path="/details", method = RequestMethod.GET)// mapear solicitudes HTTP GET a metodos controladores especificos en la clase
+  @GetMapping(path="/details") 
+  //@ResponseBody opcional 2
                       //(Map<String, Objetct> model)
   public Map<String, Object> details(){ //objeto Model como argumento que se utiliza para agregar atributos que pasaran a la vista
     Map<String, Object> body = new HashMap<>();
