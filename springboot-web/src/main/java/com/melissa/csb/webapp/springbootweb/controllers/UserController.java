c2524c2f65b4904bf41a808acd9205ea5176a9ab
DevMGcode <meli.bogar15@gmail.com>
2024-04-09 15:15:36 -0500

    El objeto Model [controlador-vista]

--- springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/UserController.java
+++ springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/UserController.java
@@ -4,15 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.melissa.csb.webapp.springbootweb.models.User;
+
 @Controller // manejará  solicitudes HTTP entrantes
 public class UserController {
 
   @GetMapping("/details")// mapear solicitudes HTTP GET a metodos controladores especificos en la clase
                       //(Map<String, Objetct> model)
   public String details(Model model){ //objeto Model como argumento que se utiliza para agregar atributos que pasaran a la vista
+    User user = new User("Melissa", "Garcia");
     model.addAttribute("title", "Hola Spring-Boot");
-    model.addAttribute("name", "Melissa");
-    model.addAttribute("lastname", "Garcia");
+    model.addAttribute("user", user);
     return "details";//nombre de la vista a renderizar
   }
 }
--- springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/UserRestController.java
@@ -5,9 +5,8 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.melissa.csb.webapp.springbootweb.models.User;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -19,15 +18,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public class UserRestController {
 
-  //@RequestMapping(path="/details", method = RequestMethod.GET)// mapear solicitudes HTTP GET a metodos controladores especificos en la clase
-  @GetMapping(path="/details") 
+  //@RequestMapping(path="/details", method = RequestMethod.GET)// mapear solicitudes HTTP GET a metodos controladores especificos en la clase opcional 2
+  @GetMapping(path="/details")  
   //@ResponseBody opcional 2
                       //(Map<String, Objetct> model)
   public Map<String, Object> details(){ //objeto Model como argumento que se utiliza para agregar atributos que pasaran a la vista
+    User user = new User("Melissa", "Garcia");
     Map<String, Object> body = new HashMap<>();
     body.put("title", "Hola Spring-Boot");
-    body.put("name", "Melissa");
-    body.put("lastname", "Garcia");
+    body.put("user", user);
     return body;//nombre de la vista a renderizar
   }
 }
