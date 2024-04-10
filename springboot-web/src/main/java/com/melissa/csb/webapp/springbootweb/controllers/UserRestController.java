5a81a5d867dda564157bb904729f095d55a8292f
DevMGcode <meli.bogar15@gmail.com>
2024-04-10 16:13:10 -0500

    La clase DTO [Controlador-claseDto]

--- springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/UserRestController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.melissa.csb.webapp.springbootweb.dto.UserDto;
 import com.melissa.csb.webapp.springbootweb.models.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,13 +22,27 @@ public class UserRestController {
 
   //@RequestMapping(path="/details", method = RequestMethod.GET)// mapear solicitudes HTTP GET a metodos controladores especificos en la clase opcional 2
   @GetMapping(path="/details")  
-  //@ResponseBody opcional 2
-                      //(Map<String, Objetct> model)
-  public Map<String, Object> details(){ //objeto Model como argumento que se utiliza para agregar atributos que pasaran a la vista
+  
+  
+  public UserDto details(){ //objeto     
     User user = new User("Melissa", "Garcia");
-    Map<String, Object> body = new HashMap<>();
-    body.put("title", "Hola Spring-Boot");
-    body.put("user", user);
-    return body;//nombre de la vista a renderizar
+    UserDto userDto= new UserDto();
+    userDto.setUser(user);
+    userDto.setTitle("Hola Spring-Boot");
+    return userDto;//nombre de la vista a renderizar
   }
+
+
+@GetMapping(path="/details-map")  
+//@ResponseBody opcional 2
+                    //(Map<String, Objetct> model)
+public Map<String, Object> detailsMap(){ //objeto Model como argumento que se utiliza para agregar atributos que pasaran a la vista
+  User user = new User("Melissa", "Garcia");
+  Map<String, Object> body = new HashMap<>();
+  body.put("title", "Hola Spring-Boot");
+  body.put("user", user);
+  return body;//nombre de la vista a renderizar
+}
+
+
 }
