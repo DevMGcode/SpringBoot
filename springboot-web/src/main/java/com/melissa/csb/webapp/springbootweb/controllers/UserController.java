30d28a1c50d2ebde5640bfe14f5eb9f9c84e8d9b
DevMGcode <meli.bogar15@gmail.com>
2024-04-10 20:30:34 -0500

    Directiva each (foreach) de Thymeleaf

--- springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/UserController.java
+++ springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/UserController.java
@@ -1,6 +1,6 @@
 package com.melissa.csb.webapp.springbootweb.controllers;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -10,31 +10,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.melissa.csb.webapp.springbootweb.models.User;
 
-
-
-@Controller // manejará  solicitudes HTTP entrantes
+@Controller
 public class UserController {
 
-  @GetMapping("/details")// mapear solicitudes HTTP GET a metodos controladores especificos en la clase
-                      //(Map<String, Objetct> model)
-  public String details(Model model){ //objeto Model como argumento que se utiliza para agregar atributos que pasaran a la vista
-    User user = new User("Melissa", "Garcia");
-    user.setEmail("Meligarcia@gmail.com");
-    model.addAttribute("title", "Hola Spring-Boot");
-    model.addAttribute("user", user);
-    return "details";//nombre de la vista a renderizar
-  }
+    @GetMapping("/details")
+    public String details(Model model) {
+        User user = new User("Melissa", "Garcia");
+        user.setEmail("Meligarcia@gmail.com");
+        model.addAttribute("title", "Hola Spring-Boot");
+        model.addAttribute("user", user);
+        return "details";
+    }
 
-  @GetMapping("/list")
-  public String list (ModelMap model) {
-    List<User> users = new ArrayList<>();
-    model.addAttribute("users",users);
-    model.addAttribute("title", "listado de usuarios");   
-    /* model.addAttribute("title","listado de usuarios"); */
-    return "list";
-  }
-  
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = Arrays.asList(
+            new User("Meli", "Garcia","mg@gmail.com"),
+            new User("Fede", "gonzalez"),
+            new User("Juliana", "Hernandez","JH@gmail.com"),
+            new User("Sebastian", "Benavidez")
+        );
 
+        model.addAttribute("users", users);
+        model.addAttribute("title", "listado de usuarios");
+        return "list";
+    }
 }
 
 
