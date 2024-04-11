88fc1bbe587d67957a38a397bb423882b0ebde00
DevMGcode <meli.bogar15@gmail.com>
2024-04-11 10:08:48 -0500

    Objeto request desde HttpServletRequest

--- springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/RequestParamsController.java
+++ springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/RequestParamsController.java
@@ -2,12 +2,10 @@ package com.melissa.csb.webapp.springbootweb.controllers;
 import org.springframework.web.bind.annotation.RestController;
 import com.melissa.csb.webapp.springbootweb.dto.ParamDto;
 import com.melissa.csb.webapp.springbootweb.dto.ParamMixDto;
-
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @RestController
 @RequestMapping("/api/params")
@@ -32,6 +30,21 @@ public class RequestParamsController {
     return params;
   }
   //http://localhost:8090/api/params/bar?text=algun%20mensaje&code=12345
+
+  @GetMapping("/request")
+  public ParamMixDto request(HttpServletRequest request){
+    Integer code=10;
+    try{
+      code = Integer.parseInt(request.getParameter("code"));
+    }catch(NumberFormatException e){
+    }
+    ParamMixDto params = new ParamMixDto();
+    params.setCode(code);
+    params.setMessage(request.getParameter("message"));
+    return params;
+  }
+  //http://localhost:8090/api/params/request?code=123&message=algun%20mensaje
+ 
   
 
 }
