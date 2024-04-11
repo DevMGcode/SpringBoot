5fe3657d64763ec3878e856d86e0f126216d8955
DevMGcode <meli.bogar15@gmail.com>
2024-04-11 09:20:44 -0500

    Anotación @RequestParam[ parametros - (RequestParamsController-ParamMixDto)]

--- springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/RequestParamsController.java
+++ springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/RequestParamsController.java
@@ -1,6 +1,8 @@
 package com.melissa.csb.webapp.springbootweb.controllers;
 import org.springframework.web.bind.annotation.RestController;
 import com.melissa.csb.webapp.springbootweb.dto.ParamDto;
+import com.melissa.csb.webapp.springbootweb.dto.ParamMixDto;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,5 +23,15 @@ public class RequestParamsController {
     return param;
   }
   //http://localhost:8090/api/params/foo?message=Hola
+
+  @GetMapping("/bar")
+  public ParamMixDto bar(@RequestParam String text, @RequestParam Integer code){
+    ParamMixDto params = new ParamMixDto();
+    params.setMessage(text);
+    params.setCode(code);
+    return params;
+  }
+  //http://localhost:8090/api/params/bar?text=algun%20mensaje&code=12345
+  
 
 }
