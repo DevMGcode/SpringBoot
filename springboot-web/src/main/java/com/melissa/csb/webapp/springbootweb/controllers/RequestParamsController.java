d07133b75b69dc70dd67dee610feacf973b291a3
DevMGcode <meli.bogar15@gmail.com>
2024-04-11 09:02:04 -0500

    Anotación @RequestParam [parametro(RestParamsController-ParamsDto)]

---
+++ springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/RequestParamsController.java
@@ -0,0 +1,25 @@
+package com.melissa.csb.webapp.springbootweb.controllers;
+import org.springframework.web.bind.annotation.RestController;
+import com.melissa.csb.webapp.springbootweb.dto.ParamDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping("/api/params")
+
+
+public class RequestParamsController {
+
+  @GetMapping("/foo") //recibe un parametro de tipo message de la url es obligatorio
+  public ParamDto foo(@RequestParam( required = false, defaultValue = "como estas") String message){
+
+    ParamDto param= new ParamDto();
+    param.setMessage(message);
+    return param;
+  }
+  //http://localhost:8090/api/params/foo?message=Hola
+
+}
---
+++ springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/dto/ParamDto.java
@@ -0,0 +1,15 @@
+package com.melissa.csb.webapp.springbootweb.dto;
+
+public class ParamDto {
+  
+  private String message;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+  
+}
