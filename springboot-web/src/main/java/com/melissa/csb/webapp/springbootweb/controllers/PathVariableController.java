dc48d56b6116529dd0b88daa4461c7701135b474
DevMGcode <meli.bogar15@gmail.com>
2024-04-12 09:57:09 -0500

    Inyectar valores usando la anotación @value

--- springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/PathVariableController.java
+++ springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/PathVariableController.java
@@ -1,22 +1,40 @@
 package com.melissa.csb.webapp.springbootweb.controllers;
-
 import org.springframework.web.bind.annotation.RestController;
 import com.melissa.csb.webapp.springbootweb.dto.ParamDto;
 import com.melissa.csb.webapp.springbootweb.models.User;
-
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+  //escritas en application.properties
+
+  @Value("${config.username}")
+  private String username;
+
+/*   @Value("${config.message}")
+      private String message; 
+*/
+
+  @Value("${config.listOfValues}")
+  private String listOfValues;
+
+  @Value("${config.code}")
+  private String code;
+
+
+
   @GetMapping("/baz/{message}")
   public ParamDto baz(@PathVariable String message) {
     ParamDto param = new ParamDto();
@@ -54,5 +72,15 @@ public class PathVariableController {
   
 */
   
-
+  @GetMapping("/values")
+  public  Map <String, Object> values(@Value("${config.message}") String message){
+    Map <String, Object> json = new HashMap<>();
+    json.put("username", username);
+    json.put("code", code);
+    json.put("message", message);
+    json.put("listOfValues", listOfValues);
+    return json;
+    //localhost:8090/api/var/values
+  }
+  
 }
