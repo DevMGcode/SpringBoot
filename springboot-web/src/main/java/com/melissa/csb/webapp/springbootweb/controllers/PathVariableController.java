9dda9d2b36f316a175bd7129d5f9b5e0cdb9f201
DevMGcode <meli.bogar15@gmail.com>
2024-04-12 08:54:06 -0500

    Obtener varios parametros de la ruta con @PathVariable

--- springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/PathVariableController.java
+++ springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/PathVariableController.java
@@ -1,23 +1,34 @@
 package com.melissa.csb.webapp.springbootweb.controllers;
+
 import org.springframework.web.bind.annotation.RestController;
-
 import com.melissa.csb.webapp.springbootweb.dto.ParamDto;
-
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
   @GetMapping("/baz/{message}")
-  public ParamDto baz(@PathVariable String message){
-    ParamDto param= new ParamDto();
+  public ParamDto baz(@PathVariable String message) {
+    ParamDto param = new ParamDto();
     param.setMessage(message);
     return param;
-    //http://localhost:8090/api/var/baz/mesa
-    //más estandar el parametro en la ruta
-  } 
+    // http://localhost:8090/api/var/baz/mesa
+    // más estandar el parametro en la ruta
+  }
+
+  @GetMapping("/mix/{product}/{id}")
+  public Map<String, Object> mixPathVariable(@PathVariable String product, @PathVariable Long id) {
+    Map<String, Object> json = new HashMap<>();
+    json.put("product", product);
+    json.put("id", id );
+
+    return json;
+    //http://localhost:8090/api/var/mix/mouse/25
+  }
+
 }
