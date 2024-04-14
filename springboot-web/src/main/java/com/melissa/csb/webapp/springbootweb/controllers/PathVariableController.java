1aede5861e1ed053004592d87c8f00002dd97df2
DevMGcode <meli.bogar15@gmail.com>
2024-04-14 13:37:09 -0500

    Objeto Environment de Spring para leer configuraciones

--- springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/PathVariableController.java
+++ springboot-web/src/main/java/com/melissa/csb/webapp/springbootweb/controllers/PathVariableController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -27,7 +30,7 @@ public class PathVariableController {
 /*   @Value("${config.message}")
       private String message; 
 */
-
+  //inyectar configuraciones
   @Value("${config.listOfValues}")
   private List<String> listOfValues;//automatica
   //private String [] listOfValues;
@@ -49,6 +52,9 @@ public class PathVariableController {
 
   @Value("#{${config.valuesMap}.price}")
   private Long price;
+
+  @Autowired //por su tipo de dato lo inyecta
+  private Environment environment;
 
   @GetMapping("/baz/{message}")
   public ParamDto baz(@PathVariable String message) {
@@ -86,13 +92,16 @@ public class PathVariableController {
   } 
   
 */
-  
-  @GetMapping("/values")
-  public  Map <String, Object> values(@Value("${config.message}") String message){
+@GetMapping("/values")
+public  Map <String, Object> values(@Value("${config.message}") String message){
+    Long code2= environment.getProperty("config.code",Long.class);
     Map <String, Object> json = new HashMap<>();
     json.put("username", username);
     json.put("code", code);
+    json.put("code2", code2);
+    //json.put("code2", Integer.valueOf(environment.getProperty("config.code")));
     json.put("message", message);
+    json.put("message2", environment.getProperty("config.message"));
     json.put("listOfValues", listOfValues);
     json.put("valueList", valueList);
     json.put("valueString", valueString);
