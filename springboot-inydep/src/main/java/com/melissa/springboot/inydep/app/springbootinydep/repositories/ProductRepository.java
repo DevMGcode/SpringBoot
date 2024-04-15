9e4757112e495f8d48f1aa90847136f7226f19d3
DevMGcode <meli.bogar15@gmail.com>
2024-04-15 16:13:21 -0500

    Implementando método findById en la clase Repository

--- springboot-inydep/src/main/java/com/melissa/springboot/inydep/app/springbootinydep/repositories/ProductRepository.java
+++ springboot-inydep/src/main/java/com/melissa/springboot/inydep/app/springbootinydep/repositories/ProductRepository.java
@@ -6,8 +6,10 @@ import java.util.List;
 import com.melissa.springboot.inydep.app.springbootinydep.models.Product;
 
 public class ProductRepository {
+  // Lista que almacena los productos
   private List<Product> data;
 
+  // Constructor de la clase que inicializa la lista 'data' con algunos productos
   public ProductRepository() {
     this.data=Arrays.asList(new Product(1L,"Memoria Kinstom 54",300L),
     new Product(2L,"CPU intel core i9",850L),
@@ -15,8 +17,20 @@ public class ProductRepository {
     new Product(4L,"Lapto Lenovo",490L));
   }
   
+  // Método para obtener todos los productos en el repositorio
   public List<Product> findALL(){
     return data;
+  }
+
+  public Product findById(Long id){
+    // Inicia un flujo sobre la lista de productos 'data'
+    return data.stream()
+    // Aplica un filtro para seleccionar solo aquellos productos cuyo ID coincide con el proporcionado
+    .filter(p-> p.getId().equals(id))
+     // Busca el primer elemento que coincide con el filtro aplicado
+    .findFirst()
+    // Obtiene el valor contenido si está presente, o devuelve null si está vacío
+    .orElse(null);
   }
 
 }
