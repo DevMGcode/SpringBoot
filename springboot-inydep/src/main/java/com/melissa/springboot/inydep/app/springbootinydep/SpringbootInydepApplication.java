75a2d9da19c668eff4b011b74f538f89b2a9a62d
DevMGcode <meli.bogar15@gmail.com>
2024-04-15 14:04:38 -0500

    Nuevo Proyecto Inyección de dependecias (IoC)

---
+++ springboot-inydep/src/main/java/com/melissa/springboot/inydep/app/springbootinydep/SpringbootInydepApplication.java
@@ -0,0 +1,13 @@
+package com.melissa.springboot.inydep.app.springbootinydep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInydepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInydepApplication.class, args);
+	}
+
+}
---
+++ springboot-inydep/src/test/java/com/melissa/springboot/inydep/app/springbootinydep/SpringbootInydepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melissa.springboot.inydep.app.springbootinydep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInydepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
