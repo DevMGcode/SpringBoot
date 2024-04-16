b2927dc303f7bc0c0ee9c9f61d830817a7d32e49
DevMGcode <meli.bogar15@gmail.com>
2024-04-16 15:50:04 -0500

    La clase Controller

---
+++ springboot-inydep/src/main/java/com/melissa/springboot/inydep/app/springbootinydep/controllers/Controller1.java
@@ -0,0 +1,27 @@
+package com.melissa.springboot.inydep.app.springbootinydep.controllers;
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+import com.melissa.springboot.inydep.app.springbootinydep.models.Product;
+import com.melissa.springboot.inydep.app.springbootinydep.services.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/api")
+public class Controller1 {
+ //localhost:8080/api
+  private ProductService service = new ProductService();
+
+  @GetMapping
+  public List<Product> list() {
+    return service.findALL();
+  }
+
+  @GetMapping("/{id}")
+  public Product show(@PathVariable Long id) {
+    return service.findById(id);
+  }
+}
+
