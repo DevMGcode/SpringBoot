daac548d895462b28f58e7624dd20156e18b91be
DevMGcode <meli.bogar15@gmail.com>
2024-04-19 19:54:07 -0500

    Algunas Variantes en el componente registrado con @Configuration y @Bean

--- springboot-inydep/src/main/java/com/melissa/springboot/inydep/app/springbootinydep/AppConfig.java
+++ springboot-inydep/src/main/java/com/melissa/springboot/inydep/app/springbootinydep/AppConfig.java
@@ -1,8 +1,10 @@
 package com.melissa.springboot.inydep.app.springbootinydep;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
 
 import com.melissa.springboot.inydep.app.springbootinydep.services.ProductRepository;
 import com.melissa.springboot.inydep.app.springbootinydep.repositories.ProductRepositoryJson;
@@ -11,12 +13,15 @@ import com.melissa.springboot.inydep.app.springbootinydep.repositories.ProductRe
 @PropertySource("classpath:config.properties")
 public class AppConfig {
 
+  @Value("classpath:json/product.json")
+  private Resource resource;
+
   @Bean("productJson")//alternativa de CmpS
   
   // Cambia el tipo de retorno a ProductRepositoryJson
   ProductRepository productRepositoryJson(){
     // Cambia la forma de instanciar ProductRepositoryJson
-    return new ProductRepositoryJson();
+    return new ProductRepositoryJson(resource);
   }
 
 }
--- springboot-inydep/src/main/java/com/melissa/springboot/inydep/app/springbootinydep/repositories/ProductRepositoryJson.java
+++ springboot-inydep/src/main/java/com/melissa/springboot/inydep/app/springbootinydep/repositories/ProductRepositoryJson.java
@@ -7,8 +7,7 @@ import java.util.List;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.melissa.springboot.inydep.app.springbootinydep.models.Product;
 import com.melissa.springboot.inydep.app.springbootinydep.services.ProductRepository;
@@ -17,21 +16,26 @@ public class ProductRepositoryJson implements ProductRepository {
 
   private List<Product> list; // Declara una lista de productos
 
+  
+
   // Constructor de la clase ProductRepositoryJson
   public ProductRepositoryJson(){
-    // Obtiene una referencia al archivo de recursos "product.json" en el directorio de recursos
+    
     Resource resource = new ClassPathResource("json/product.json");
-    // Crea un ObjectMapper, que se utiliza para leer y escribir JSON
+    readValueJson(resource);  
+  }
+
+  public ProductRepositoryJson(Resource resource){
+    readValueJson(resource);
+  }
+
+  public void readValueJson(Resource resource){
     ObjectMapper objectMapper = new ObjectMapper(); 
     try {
-      // Lee el contenido del archivo JSON y lo mapea a un array de objetos de tipo Product
-      list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
-    } catch (StreamReadException e) { // Captura excepciones relacionadas con la lectura del flujo de entrada
-      e.printStackTrace(); // Imprime la traza de la excepción en la consola
-    } catch (DatabindException e) { // Captura excepciones relacionadas con el mapeo de datos
-      e.printStackTrace(); // Imprime la traza de la excepción en la consola
-    } catch (IOException e) { // Captura excepciones de entrada/salida
-      e.printStackTrace(); // Imprime la traza de la excepción en la consola
+    
+      list = Arrays.asList(objectMapper.readValue(resource.getInputStream(), Product[].class));
+    } catch (IOException e) { 
+      e.printStackTrace(); 
     }
   }
 
