4aed99177757f0a2995bf470c740b5e4eb378f4a
DevMGcode <meli.bogar15@gmail.com>
2024-04-20 10:15:13 -0500

    Factura y relaciones entre sus componentes

--- springboot-inydepfactura/src/main/java/com/melissa/springboot/inydep/factura/springbootinydepfactura/models/Client.java
+++ springboot-inydepfactura/src/main/java/com/melissa/springboot/inydep/factura/springbootinydepfactura/models/Client.java
@@ -1,5 +1,23 @@
 package com.melissa.springboot.inydep.factura.springbootinydepfactura.models;
 
-public class Client {
+import org.springframework.stereotype.Component;
 
+@Component
+public class Client {
+  
+  private String name;
+  private String lastaname;
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getLastaname() {
+    return lastaname;
+  }
+  public void setLastaname(String lastaname) {
+    this.lastaname = lastaname;
+  }
+  
 }
--- springboot-inydepfactura/src/main/java/com/melissa/springboot/inydep/factura/springbootinydepfactura/models/item.java
+++ springboot-inydepfactura/src/main/java/com/melissa/springboot/inydep/factura/springbootinydepfactura/models/item.java
@@ -1,5 +1,21 @@
 package com.melissa.springboot.inydep.factura.springbootinydepfactura.models;
 
-public class item {
+public class Item {
+  private Product product;
+  private Integer quantity;
+  
+  public Product getProduct() {
+    return product;
+  }
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+  public Integer getQuantity() {
+    return quantity;
+  }
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
 
+  
 }
