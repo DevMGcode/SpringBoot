38fc4c3e0975f0b41e870fa7d487fa4b7e072183
DevMGcode <meli.bogar15@gmail.com>
2024-04-22 15:52:29 -0500

    Añadiendo método handler para el manejo de excepción

--- springboot-error/src/main/java/com/melissa/springboot/error/springbooterror/controllers/HandlerExceptionController.java
+++ springboot-error/src/main/java/com/melissa/springboot/error/springbooterror/controllers/HandlerExceptionController.java
@@ -1,5 +1,9 @@
 package com.melissa.springboot.error.springbooterror.controllers;
+import com.melissa.springboot.error.springbooterror.models.Error;
+import java.util.Date;
 
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,9 +11,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class HandlerExceptionController {
 
-  @ExceptionHandler(ArithmeticException.class)
-  public ResponseEntity<?> divisionByZero(Exception ex){
-    return  ResponseEntity.internalServerError().body("Error 500");
+  @ExceptionHandler({ArithmeticException.class})
+  public ResponseEntity<Error> divisionByZero(Exception ex){
+    Error error = new Error();
+    error.setDate(new Date());
+    error.setError("Error en division en cero");
+    error.setMessage(ex.getMessage());
+    error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    //return  ResponseEntity.internalServerError().body(error);
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+
   }
 
 }
--- springboot-error/src/main/java/com/melissa/springboot/error/springbooterror/models/Error.java
+++ springboot-error/src/main/java/com/melissa/springboot/error/springbooterror/models/Error.java
@@ -1,6 +1,6 @@
 package com.melissa.springboot.error.springbooterror.models;
 
-import java.sql.Date;
+import java.util.Date;
 
 public class Error {
   
