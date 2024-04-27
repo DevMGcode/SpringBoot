379e2406708a96937ae47a3c92ce5d5d26e6bbf9
DevMGcode <meli.bogar15@gmail.com>
2024-04-26 20:57:24 -0500

    Implementando interceptor TiempoTranscurrido

--- springboot-interceptor/src/main/java/com/example/melissa/springboot/app/interceptor/springbootinterceptor/interceptors/LoadingTimeInterceptor.java
+++ springboot-interceptor/src/main/java/com/example/melissa/springboot/app/interceptor/springbootinterceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,7 @@
 package com.example.melissa.springboot.app.interceptor.springbootinterceptor.interceptors;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
@@ -15,19 +17,32 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component("timeInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor{
 
-  private static final Logger logger= LoggerFactory.getLogger(LoadingTimeInterceptor.class);
-  @Override
-  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-      @Nullable ModelAndView modelAndView) throws Exception {
-    logger.info("LoadingTimeInterceptor: posHandle() saliendo...." +((HandlerMethod)handler).getMethod().getName() );
-    
-  }
   
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
     HandlerMethod controls = ((HandlerMethod)handler);
     logger.info("LoadingTimeInterceptor: preHandle() entrando...." + controls.getMethod().getName());
+
+    long star = System.currentTimeMillis();
+    request.setAttribute("start", star);
+
+    Random random = new Random();
+    int delay = random.nextInt(500);
+    Thread.sleep(delay);
     return true;
   }
   
+  private static final Logger logger= LoggerFactory.getLogger(LoadingTimeInterceptor.class);
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+      @Nullable ModelAndView modelAndView) throws Exception {
+    
+    long end = System.currentTimeMillis();
+    long star = (long) request.getAttribute("start");
+    long result = end -star;
+    logger.info("Tiempo trascurrido: "+ result + " milisegundos!");
+    logger.info("LoadingTimeInterceptor: posHandle() saliendo...." +((HandlerMethod)handler).getMethod().getName() );
+    
+  }
+
 }
