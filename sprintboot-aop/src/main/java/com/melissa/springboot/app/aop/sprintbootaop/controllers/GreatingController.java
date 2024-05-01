5c7c13a7c9a508f0d089a9e217a328d1d61a6e0c
DevMGcode <meli.bogar15@gmail.com>
2024-05-01 18:13:51 -0500

    Añadiendo componente Service

--- sprintboot-aop/src/main/java/com/melissa/springboot/app/aop/sprintbootaop/controllers/GreatingController.java
+++ sprintboot-aop/src/main/java/com/melissa/springboot/app/aop/sprintbootaop/controllers/GreatingController.java
@@ -3,16 +3,21 @@ package com.melissa.springboot.app.aop.sprintbootaop.controllers;
 
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.melissa.springboot.app.aop.sprintbootaop.services.GreetingService;
+
 @RestController
 public class GreatingController {
 
+  @Autowired
+  private GreetingService greeatingService;
   @GetMapping("/greeting")
   public ResponseEntity <?> greeting(){
 
-    return ResponseEntity.ok(Collections.singletonMap("greeting", null));
+    return ResponseEntity.ok(Collections.singletonMap("greeting", greeatingService.sayHello("Meli", "Hola que tal!")));
   }
 }
---
+++ sprintboot-aop/src/main/java/com/melissa/springboot/app/aop/sprintbootaop/services/GreetingService.java
@@ -0,0 +1,7 @@
+package com.melissa.springboot.app.aop.sprintbootaop.services;
+
+
+public interface GreetingService {
+  String sayHello(String person, String phrase);
+  
+} 
