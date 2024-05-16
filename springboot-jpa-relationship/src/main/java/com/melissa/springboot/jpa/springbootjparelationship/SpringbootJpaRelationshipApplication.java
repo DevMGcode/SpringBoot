da90b520ee73ca8a02c43017b86fa03eb3c09c4b
DevMGcode <meli.bogar15@gmail.com>
2024-05-16 09:01:09 -0500

    La relacion OneToMany

--- springboot-jpa-relationship/src/main/java/com/melissa/springboot/jpa/springbootjparelationship/SpringbootJpaRelationshipApplication.java
+++ springboot-jpa-relationship/src/main/java/com/melissa/springboot/jpa/springbootjparelationship/SpringbootJpaRelationshipApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.melissa.springboot.jpa.springbootjparelationship.entities.Client;
 import com.melissa.springboot.jpa.springbootjparelationship.entities.Invoice;
@@ -28,6 +29,7 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
 		manyToOneFindByIdClient();
 	}
 
+	@Transactional
 	public void manyToOne(){
 		Client client = new Client ("Daniel","Restrepo");
 		clientRepository.save(client);
@@ -38,14 +40,11 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
 		System.out.println(invoiceDB);
 	}
 
+	@Transactional
 	public void manyToOneFindByIdClient(){
-
 		Optional <Client>  optionalClient = clientRepository.findById(1L);
-		
-		if(optionalClient.isPresent()){
-
+			if(optionalClient.isPresent()){
 			Client client = optionalClient.orElseThrow();
-
 			Invoice invoice = new Invoice("Compras de oficina",2000L);
 			invoice.setClient(client);
 			Invoice invoiceDB = invoiceRepository.save(invoice);
