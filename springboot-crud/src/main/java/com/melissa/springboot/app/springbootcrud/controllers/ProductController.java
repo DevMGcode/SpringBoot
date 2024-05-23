12451b189ee713fe1e1c927f10a4d2d18cdc2cf1
DevMGcode <meli.bogar15@gmail.com>
2024-05-23 17:07:15 -0500

    Personalizando la respuesta con los mensajes de error

--- springboot-crud/src/main/java/com/melissa/springboot/app/springbootcrud/controllers/ProductController.java
+++ springboot-crud/src/main/java/com/melissa/springboot/app/springbootcrud/controllers/ProductController.java
@@ -1,11 +1,14 @@
 package com.melissa.springboot.app.springbootcrud.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,12 +49,19 @@ public class ProductController {
   }
 
   @PostMapping
-  public ResponseEntity<Product> create(@Valid @RequestBody Product product){
+  public ResponseEntity<?> create(@Valid @RequestBody Product product, BindingResult result){
+    if(result.hasFieldErrors()){
+      return validation(result);
+    }
     return ResponseEntity.status(HttpStatus.CREATED).body(service.save(product));
   }
 
   @PutMapping("/{id}")
-    public ResponseEntity<Product> update( @PathVariable Long id, @Valid @RequestBody Product product){      
+    public ResponseEntity<?> update(  @Valid @RequestBody Product product,BindingResult result, @PathVariable Long id){      
+    if(result.hasFieldErrors()){
+      return validation(result);
+    }
+
     Optional<Product> productOptional = service.update(id,product);
     if (productOptional.isPresent()) {      
       return ResponseEntity.status(HttpStatus.CREATED).body(productOptional.orElseThrow());
@@ -59,9 +69,10 @@ public class ProductController {
     return ResponseEntity.notFound().build();
   }
 
+  
   @DeleteMapping("/{id}")
   public ResponseEntity<?> delete(@PathVariable Long id){
-  /*   Product product = new Product();
+    /*   Product product = new Product();
     product.setId(id); */
     Optional<Product> productOptional = service.delete(id);
     if(productOptional.isPresent()){
@@ -71,5 +82,14 @@ public class ProductController {
     return ResponseEntity.notFound().build();
   }
   
+  
+  private ResponseEntity<?> validation(BindingResult result) {
+    Map<String, String> errors = new HashMap<>();
+
+    result.getFieldErrors().forEach(err->{
+      errors.put(err.getField(), "El campo "+ err.getField()+" "+ err.getDefaultMessage());
+    });;
+    return ResponseEntity.badRequest().body(errors);
+  }
 
 }
